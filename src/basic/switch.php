c870d4ad5e6e070e1497c692c08dba5eebceb87a
esteban <tebas_tebas@hotmail.com>
2021-06-01 13:59:26 +0200

    ficheros

---
+++ src/basic/switch.php
@@ -0,0 +1,15 @@
+<?php
+
+switch($optionId){
+  case (1):
+    $optionText = "Leer menú";
+    break;
+  case (2):
+    $optionText = "Editar menú";
+    break;
+  case (3):
+    $optionText = "Eliminar menú";
+    break;
+  default:
+    $optionText = "Opción no disponible";
+    break;
